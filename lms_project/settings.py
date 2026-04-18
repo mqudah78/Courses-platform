e673fb972e9698d63468226a4f7bb976d1ebc3c4
Mohammed Qudah <mqudah978@gmail.com>
2026-04-18 23:03:46 +0300

    Fix gitignore

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =========================
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-secret-key')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'False'
 
 
 ALLOWED_HOSTS = [
