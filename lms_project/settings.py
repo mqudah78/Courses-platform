3ed0574f761b551fca8f11ddcfd0c7d8b53bb437
Mohammed Qudah <mqudah978@gmail.com>
2026-08-01 12:10:35 +0300

    Update Django LMS project environment

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Whitenoise (important for Render)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
